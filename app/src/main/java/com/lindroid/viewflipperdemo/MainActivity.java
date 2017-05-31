c937ed736869550455b028b66a5da5dbfb42e829
Lindroy <linyulong2014@163.com>
2017-05-31 15:31:24 +0800

    实现从左往右滑动的动画效果。

--- app/src/main/java/com/lindroid/viewflipperdemo/MainActivity.java
+++ app/src/main/java/com/lindroid/viewflipperdemo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.lindroid.viewflipperdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewFlipper viewFlipper;
+    //要添加的页面布局ID
+    private int viewIds[] = {R.layout.item_view1, R.layout.item_view2, R.layout.item_view3, R.layout.item_view4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViewFlipper();
+//        addViews();
     }
+
+    private void initViewFlipper() {
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+//        viewFlipper.setOnTouchListener(this);
+    }
+
+    /**
+     * 将页面添加进ViewFlipper
+     */
+    private void addViews() {
+        View itemView;
+        for (int viewId : viewIds) {
+            itemView = View.inflate(this, viewId, null);
+            viewFlipper.addView(itemView);
+        }
+    }
+
 }
