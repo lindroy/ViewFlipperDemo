7e964623de1ee2b7f36a9d2fb5801b8a256de4df
Lindroid <linyulong2014@163.com>
2017-05-31 15:23:42 +0800

    创建工程。

---
+++ app/src/main/java/com/lindroid/viewflipperdemo/ListActivity.java
@@ -0,0 +1,24 @@
+package com.lindroid.viewflipperdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+    /**
+     * Android之ViewFlipper的简单使用
+     *
+     * @param view
+     */
+    public void normalClick(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
