450f9241be458633ae14ec140efb576266bdae4e
Lindroy <linyulong2014@163.com>
2017-05-31 16:39:18 +0800

    使用触摸监听事件实现了手指滑屏。

--- app/src/main/java/com/lindroid/viewflipperdemo/MainActivity.java
+++ app/src/main/java/com/lindroid/viewflipperdemo/MainActivity.java
@@ -2,13 +2,17 @@ package com.lindroid.viewflipperdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
     private ViewFlipper viewFlipper;
     //要添加的页面布局ID
     private int viewIds[] = {R.layout.item_view1, R.layout.item_view2, R.layout.item_view3, R.layout.item_view4};
+    private float startX; //手指按下时的x坐标
+    private float endX; //手指抬起时的x坐标
+    private float moveX = 100f; //判断是否切换页面的标准值
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViewFlipper() {
         viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
-//        viewFlipper.setOnTouchListener(this);
+        viewFlipper.setOnTouchListener(this);
     }
 
     /**
@@ -32,6 +36,39 @@ public class MainActivity extends AppCompatActivity {
             itemView = View.inflate(this, viewId, null);
             viewFlipper.addView(itemView);
         }
+    }
+
+
+    /**
+     * 触摸监听事件
+     *
+     * @param v
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //手指按下时获取起始点坐标
+                startX = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                //手指抬起时获取结束点坐标
+                endX = event.getX();
+                //比较startX和endX，判断手指的滑动方向
+                if (endX - startX > moveX) { //手指从左向右滑动
+                    viewFlipper.setInAnimation(this, R.anim.left_in);
+                    viewFlipper.setOutAnimation(this, R.anim.right_out);
+                    viewFlipper.showPrevious();
+                } else if (startX - endX > moveX) { //手指向右向左滑动
+                    viewFlipper.setInAnimation(this, R.anim.right_in);
+                    viewFlipper.setOutAnimation(this, R.anim.left_out);
+                    viewFlipper.showNext();
+                }
+                break;
+        }
+        return true;
     }
 
 }
